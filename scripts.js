31392d33c86e4fb36bddfb9a73fcaf7ab3d9e9e6
Christian Bustos <christianbus37@gmail.com>
2023-03-31 17:10:02 -0300

    fix problem with number

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@ function calcularLitros(){
     let textoKm = elementoKm.value;
     let cantKm = Number(textoKm);
 
-    let cantLitros = Math.trunc(cantKm / 8.8);
+    let cantLitros = parseFloat(cantKm / 8.8).toFixed(2);
 
     let resultado = document.getElementById("textoResultado");
     resultado.textContent = "Carga " + cantLitros + " litros de combustible";
